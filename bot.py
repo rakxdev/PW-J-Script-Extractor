3b1427f96bc10be538c47b14bc2c63a237877428
Rak X Dev. <rakesh.943803@gmail.com>
2025-08-07 14:32:05 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -47,13 +47,13 @@ from telegram.ext import (
 )
 
 # Configuration
-TOKEN = "7760431264:AAEhbngfFwxMp2nmD6jzzYvPoBUPBMGW8e0"
-AUTHORIZED_USERS = {
-    7875474866,
-    1955134660,
-    1429154571,
-    7073085399  # Replace with your numeric Telegram user IDs
-}
+# TOKEN = "7760431264:AAEhbngfFwxMp2nmD6jzzYvPoBUPBMGW8e0"
+# AUTHORIZED_USERS = {
+#     7875474866,
+#     1955134660,
+#     1429154571,
+#     7073085399  # Replace with your numeric Telegram user IDs
+# }
 
 # Logging configuration
 logging.basicConfig(
